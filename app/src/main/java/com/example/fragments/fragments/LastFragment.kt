cd1661ecd44ed77032de8ff7092accfb5698046e
SaBa112112 <saba.khujadze.1@btu.edu.ge>
2022-12-01 18:57:29 +0400

    fragments1

---
+++ app/src/main/java/com/example/fragments/fragments/LastFragment.kt
@@ -0,0 +1,12 @@
+package com.example.fragments.fragments
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.fragments.R
+
+class LastFragment : Fragment(R.layout.fragment_last) {
+
+}
